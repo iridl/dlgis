06d06696bba50894abb2ee0c1acffca79b7b360f
Igor Khomyakov <ikh@iri.columbia.edu>
2020-05-11 13:18:27 -0400

    Do not interrupt pipeline job on first failure

--- bitbucket-pipeline-1.sh
+++ bitbucket-pipeline-1.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
-black --check . &&\
-mypy --strict . &&\
-flake8 dlgis *.py &&\
+
+function ret {
+    return $1
+}
+
+echo "black:"
+black --check .
+ret_black=$?
+
+echo "mypy:"
+mypy --strict .
+ret_mypy=$?
+
+echo "flake8:"
+flake8 dlgis *.py
+ret_flake8=$?
+
+echo "pylint:"
 pylint dlgis *.py
+ret_pylint=$?
+
+ret $ret_black &&\
+ret $ret_mypy &&\
+ret $ret_flake8 &&\
+ret $ret_pylint
 ret_code=$?
 
-echo ""
+echo "result:"
 if [ $ret_code -eq 0 ]; then
     echo "SUCCESS"
 else
